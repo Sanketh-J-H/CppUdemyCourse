d41c748cc8353d470ec979d5eda99fdbbc8776ed
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-10 05:05:49 +0530

    new file:   BaseClassInitialization/main 	modified:   BaseClassInitialization/main.cpp

--- Section15/BaseClassInitialization/main.cpp
+++ Section15/BaseClassInitialization/main.cpp
@@ -4,40 +4,51 @@
 
 using namespace std;
 
-class Base {
+class Base
+{
 private:
     int value;
+
 public:
-   Base() : value {0}  { 
-            cout << "Base no-args constructor" << endl; 
+    Base() : value{0}
+    {
+        cout << "Base no-args constructor" << endl;
     }
-    Base(int x)  : value {x} { 
-            cout << "Base (int) overloaded constructor" << endl;
+    Base(int x) : value{x}
+    {
+        cout << "Base (int) overloaded constructor" << endl;
     }
-   ~Base() { 
-       cout << "Base destructor" << endl;
+    ~Base()
+    {
+        cout << "Base destructor" << endl;
     }
 };
 
-class Derived : public Base {
+class Derived : public Base
+{
 private:
     int doubled_value;
+
 public:
     Derived()
-        :Base {}, doubled_value {0} {
-            cout << "Derived no-args constructor " << endl; 
+        : Base{}, doubled_value{0}
+    {
+        cout << "Derived no-args constructor " << endl;
     }
-    Derived(int x) 
-        :  Base{x},  doubled_value {x * 2} { 
-            cout << "Derived (int) constructor" << endl; 
+    Derived(int x)
+        : Base{x}, doubled_value{x * 2}
+    {
+        cout << "Derived (int) constructor" << endl;
     }
-    ~Derived() { 
-        cout << "Derived destructor " << endl; 
-    } 
+    ~Derived()
+    {
+        cout << "Derived destructor " << endl;
+    }
 };
 
-int main() {
-   //  Derived d;
-   Derived d {1000};
+int main()
+{
+    Derived d;
+    // Derived d{1000};
     return 0;
 }
