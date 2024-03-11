8ad6dd3fdc8558ce27087d6be6c88d1115278178
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-11 17:09:56 +0530

    modified:   Section15/Challenge/Checking_Account.h 	Section15/Challenge/Checking_Account.cpp

--- Section15/Challenge/Checking_Account.h
+++ Section15/Challenge/Checking_Account.h
@@ -2,20 +2,22 @@
 #define _CHECKING_ACCOUNT_
 
 #include "Account.h"
+#include <iostream>
+#include <string>
 
-class Checking_Account: public Account {
+class Checking_Account : public Account
+{
     friend std::ostream &operator<<(std::ostream &os, const Checking_Account &account);
+
 private:
     static constexpr const char *def_name = "Unnamed Checking Account";
     static constexpr double def_balance = 0.0;
-    static constexpr double def_fee = 1.50;
-protected:
-    double int_rate;
+    static constexpr double per_check_fee = 1.50;
+
 public:
-    Checking_Account(std::string name = def_name, double balance =def_balance, double fee = def_fee );    
-    bool deposit(double amount);
+    Checking_Account(std::string name = def_name, double balance = def_balance);
+    bool withdraw(double amount);
     // Inherits the Account::withdraw methods
 };
-
 
 #endif
