10fb5ea155766d08cbf4191b11caa5161f8c88e3
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-12 06:30:05 +0530

    modified:   main.cpp 	main

--- Section16/TheProblem/main.cpp
+++ Section16/TheProblem/main.cpp
@@ -3,45 +3,48 @@
 #include <iostream>
 #include <memory>
 
-class Base {
+class Base
+{
 public:
-    void say_hello() const {
+    void say_hello() const
+    {
         std::cout << "Hello - I'm a Base class object" << std::endl;
     }
 };
 
-class Derived: public Base {
+class Derived : public Base
+{
 public:
-    void say_hello()  const {   
+    void say_hello() const
+    {
         std::cout << "Hello - I'm a Derived class object" << std::endl;
     }
 };
 
-void greetings(const Base &obj) {
-    std::cout << "Greetings: " ;
+void greetings(const Base &obj)
+{
+    std::cout << "Greetings: ";
     obj.say_hello();
 }
 
-int main() {
+int main()
+{
     Base b;
     b.say_hello();
-    
+
     Derived d;
     d.say_hello();
-    
+
     greetings(b);
     greetings(d);
-    
+
     Base *ptr = new Derived();
     ptr->say_hello();
-    
+
     std::unique_ptr<Base> ptr1 = std::make_unique<Derived>();
     ptr1->say_hello();
-    
-    
-    
-   delete ptr;
-    
+
+    delete ptr;
+
     return 0;
 }
-
