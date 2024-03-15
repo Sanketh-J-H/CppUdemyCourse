3a252c67365aca7eb5e3d408207de17541c4095b
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-15 07:51:07 +0530

    Section 16 Challenge in progress

--- Section16/Challenge/Trust_Account.h
+++ Section16/Challenge/Trust_Account.h
@@ -3,8 +3,10 @@
 
 #include "Savings_Account.h"
 
-class Trust_Account : public Savings_Account {
+class Trust_Account : public Savings_Account
+{
     friend std::ostream &operator<<(std::ostream &os, const Trust_Account &account);
+
 private:
     static constexpr const char *def_name = "Unnamed Trust Account";
     static constexpr double def_balance = 0.0;
@@ -13,14 +15,16 @@ private:
     static constexpr double bonus_threshold = 5000.0;
     static constexpr int max_withdrawals = 3;
     static constexpr double max_withdraw_percent = 0.2;
+
 protected:
     int num_withdrawals;
+
 public:
-    Trust_Account(std::string name = def_name,  double balance = def_balance, double int_rate = def_int_rate);
-    
+    Trust_Account(std::string name = def_name, double balance = def_balance, double int_rate = def_int_rate);
+
     // Deposits of $5000.00 or more will receive $50 bonus
     bool deposit(double amount);
-    
+
     // Only allowed maximum of 3 withdrawals, each can be up to a maximum of 20% of the account's value
     bool withdraw(double amount);
 };
