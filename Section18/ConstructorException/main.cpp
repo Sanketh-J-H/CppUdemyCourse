dfa8067a4d782b8a5236d0788123509ffcad8fd7
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-25 05:31:07 +0530

    new file:   main 	modified:   main.cpp 	new file:   ../MPGFunctionMultipleExceptions/main 	modified:   ../MPGFunctionMultipleExceptions/main.cpp 	new file:   ../MilesPerGallonFunction/main 	modified:   ../MilesPerGallonFunction/main.cpp 	new file:   ../MilesPerGallonFunctionExceptionClasses/main 	modified:   ../MilesPerGallonFunctionExceptionClasses/main.cpp 	new file:   ../StackUnwinding/main 	modified:   ../StackUnwinding/main.cpp

--- Section18/ConstructorException/main.cpp
+++ Section18/ConstructorException/main.cpp
@@ -9,9 +9,11 @@
 
 using namespace std;
 
-int main() {
-    try {
-        std::unique_ptr<Account> moes_account = std::make_unique<Checking_Account>("Moe", -10.0);
+int main()
+{
+    try
+    {
+        std::unique_ptr<Account> moes_account = std::make_unique<Checking_Account>("Moe", 10.0);
         std::cout << *moes_account << std::endl;
     }
     catch (const IllegalBalanceException &ex)
@@ -21,4 +23,3 @@ int main() {
     std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
-
