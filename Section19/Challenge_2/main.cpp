c2ca861df0543e5ce231dbfed3ff309af78e8d5d
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-28 07:20:16 +0530

    Section19 Continued

--- Section19/Challenge_2/main.cpp
+++ Section19/Challenge_2/main.cpp
@@ -2,13 +2,43 @@
 // Challenge 2
 // Automated Grader
 #include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    
-    
-    cout << "Hello world" << endl;
-    return 0;
+void ruler()
+{
+    cout << "\n123456789012345678901324567890" << endl;
 }
 
+int main()
+{
+
+    ruler();
+    cout << setw(15) << left << "Student"
+         << setw(15) << right << "Score"
+         << endl;
+    cout << setw(30) << setfill('-') << "" << endl;
+
+    ifstream in_file;
+    string word;
+    int score{0};
+
+    in_file.open("responses.txt");
+    if (!in_file)
+    {
+        cerr << "Problem opening file" << endl;
+        return 1;
+    }
+    while (in_file >> word)
+    {
+        cout << setw(15) << left << left << word
+             << setw(15) << right << score
+             << endl;
+    }
+    in_file.close();
+
+    return 0;
+}
