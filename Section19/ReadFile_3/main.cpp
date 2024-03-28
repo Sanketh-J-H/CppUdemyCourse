5e87896d066f9d3448e350dd003309129ad2b50e
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-28 06:21:03 +0530

    Section19 Continued

--- Section19/ReadFile_3/main.cpp
+++ Section19/ReadFile_3/main.cpp
@@ -4,18 +4,21 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
+int main()
+{
     std::ifstream in_file;
-    in_file.open("../poem.txt");
-    if (!in_file) {
+    in_file.open("poem.txt");
+    if (!in_file)
+    {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
     std::string line{};
-    while (std::getline(in_file, line)) {
+    while (std::getline(in_file, line))
+    {
         std::cout << line << std::endl;
     }
-    
+
     in_file.close();
     return 0;
 }
--- Section19/ReadFile_4/main.cpp
+++ Section19/ReadFile_4/main.cpp
@@ -4,16 +4,19 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
+int main()
+{
     std::ifstream in_file;
-    in_file.open("../poem.txt");
-    if (!in_file) {
+    in_file.open("poem.txt");
+    if (!in_file)
+    {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
 
-    char c {};
-    while (in_file.get(c)) {
+    char c{};
+    while (in_file.get(c))
+    {
         std::cout << c;
     }
     std::cout << std::endl;
