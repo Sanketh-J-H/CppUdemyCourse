26a87ac7dc44f863c95a484a82a082fe4b3f3041
Sanketh-J-H <sankethjh007@gmail.com>
2024-03-30 05:48:02 +0530

    Section19 Continued

--- Section19/WriteFile_1/main.cpp
+++ Section19/WriteFile_1/main.cpp
@@ -4,9 +4,11 @@
 #include <fstream>
 #include <string>
 
-int main()  {
-    std::ofstream out_file {"../output.txt", std::ios::app};
-    if (!out_file) {
+int main()
+{
+    std::ofstream out_file{"output.txt", std::ios::app};
+    if (!out_file)
+    {
         std::cerr << "Error creating file" << std::endl;
         return 1;
     }
@@ -14,9 +16,8 @@ int main()  {
     std::cout << "Enter something to write to the file: ";
     std::getline(std::cin, line);
     out_file << line << std::endl;
-    
+
     out_file.close();
-   
+
     return 0;
 }
-
