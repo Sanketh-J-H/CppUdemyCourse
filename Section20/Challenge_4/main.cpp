bb51117fca410e4e9886e1964d18570ad4def50c
Sanketh-J-H <sankethjh007@gmail.com>
2024-04-13 07:19:14 +0530

    Section20 challenge 4 complete

--- Section20/Challenge_4/main.cpp
+++ Section20/Challenge_4/main.cpp
@@ -8,23 +8,48 @@
 #include <string>
 #include <iomanip>
 
-bool is_palindrome(const std::string& s)
+bool is_palindrome(const std::string &s)
 {
     // You must implement this function.
     // Since we are learning the STL - use a stack and a queue to solve the problem.
-    return false;
+    std::stack<char> temp1;
+    std::queue<char> temp2;
+
+    for (const char &c : s)
+    {
+        if (isalpha(c))
+        {
+            temp1.push(toupper(c));
+            temp2.push(toupper(c));
+        }
+    }
+
+    char c1{}, c2{};
+    while (temp1.size() > 0)
+    {
+        c1 = temp1.top();
+        c2 = temp2.front();
+        temp1.pop();
+        temp2.pop();
+        if (c1 != c2)
+            return false;
+    }
+
+    return true;
 }
 
 int main()
 {
-    std::vector<std::string> test_strings{ "a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
-        "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
-        "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome" };
-   
+    std::vector<std::string> test_strings{"a", "aa", "aba", "abba", "abbcbba", "ab", "abc", "radar", "bob", "ana",
+                                          "avid diva", "Amore, Roma", "A Toyota's a toyota", "A Santa at NASA", "C++",
+                                          "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama!", "This is a palindrome", "palindrome"};
+
     std::cout << std::boolalpha;
-    std::cout << std::setw(8) << std::left << "Result" << "String" << std::endl;
-    for(const auto& s : test_strings) {
-        std::cout << std::setw(8) << std::left << is_palindrome(s)  << s << std::endl;
+    std::cout << std::setw(8) << std::left << "Result"
+              << "String" << std::endl;
+    for (const auto &s : test_strings)
+    {
+        std::cout << std::setw(8) << std::left << is_palindrome(s) << s << std::endl;
     }
     std::cout << std::endl;
     return 0;
